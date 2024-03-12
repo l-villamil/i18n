79d17b35bbc1a7f832153eac5e1e049958f0cc91
l-villamil <l.villamil@uniandes.edu.co>
2024-03-12 10:46:21 -0500

    done

--- src/components/job.js
+++ src/components/job.js
@@ -1,14 +1,36 @@
 import React from "react";
+import {FormattedDate, FormattedMessage, FormattedNumber, FormattedPlural} from 'react-intl';
+
 
 const Job = (props) => {
+  const salario=<td><FormattedNumber
+    value={props.offer.salary} /></td>
   return (
     <tr>
       <th scope="row">{props.offer.id}</th>
       <td>{props.offer.name}</td>
       <td>{props.offer.company}</td>
-      <td>{props.offer.salary}</td>
+      <td>
+        
+        {salario}
+        <FormattedPlural value={props.offer.salary} one=<FormattedMessage id="millionUnit"/> other=<FormattedMessage id="millionsUnit"/>/>
+        </td>
       <td>{props.offer.city}</td>
-      <td>{props.offer.date}</td>
+      
+      <td>
+  <FormattedDate
+    value={new Date(props.offer.date)}
+    year='numeric'
+    month='long'
+    day='numeric'
+    weekday='long'
+  />
+</td>
+<td>
+  <FormattedNumber
+  value={props.offer.views}  
+  />
+  </td>
     </tr>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import localeEsMessages from "./locales/es.json";
+import localeEnMessages from "./locales/en.json"
+
+import {IntlProvider} from 'react-intl';
 
 import JobsList from "./components/jobslist";
 
-ReactDOM.render(<JobsList />, document.getElementById("root"));
+const userLanguage = navigator.language.split('-')[0]; // Obtiene el idioma del navegador del usuario
+const messages = {
+    'es': localeEsMessages,
+    'en': localeEnMessages,
+    // Agrega más idiomas según sea necesario
+  };
+ReactDOM.render(     <IntlProvider locale={userLanguage} messages={messages[userLanguage]}>
+<JobsList/>
+</IntlProvider>, document.getElementById("root"));
